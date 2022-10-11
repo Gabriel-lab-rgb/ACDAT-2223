d86f565157c2dbb02de8dd4fa4cfe2c27c76498f
Gabriel-lab-rgb <grivote615@iesmartinezm.es>
2022-10-11 17:18:17 +0200

    Commit

--- ud-1-ficheros/rivera-ad-t1-proyecto/src/ad/t1/proyecto/model/ClienteApp.java
+++
@@ -1,8 +0,0 @@
-package ad.t1.proyecto.model;
-
-public class ClienteApp {
-
-    public static void main(String[] args) {
-
-    }
-}
--- ud-1-ficheros/rivera-ad-t1-proyecto/src/rivera/GestionaClientes.java
+++ ud-1-ficheros/rivera-ad-t1-proyecto/src/rivera/GestionaClientes.java
@@ -1,8 +1,50 @@
 package rivera;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class GestionaClientes {
 
     public static void main(String[] args) {
-        
+
+        generarFichero("res" + File.separator + "clientes1.csv");
     }
-}
+    public  static boolean generarFichero(String fcsv){
+
+
+        try {
+            File f = new File(fcsv);
+
+            if(f.exists()){
+                DataInputStream fichero = new DataInputStream(new FileInputStream(f));
+                BufferedInputStream buffer=new BufferedInputStream(fichero);
+                int linea=buffer.read();
+                while(linea!= -1) {
+                    System.out.print((char)linea);
+
+                    /*String[] split =String.valueOf((char)linea).split(",");
+                    System.out.println(split.length);
+                    System.out.println(Arrays.asList(split));*/
+
+                    /*System.out.print("Apellido1 : " + fichero.readUTF());
+                    System.out.print("Apellido2 : " + fichero.readUTF());
+                    System.out.println(" - Telefono: " + fichero.readInt());*/
+                    linea=buffer.read();
+                }
+
+                // Cierre del fichero
+                fichero.close();
+            }
+
+            } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return false;
+
+    }
+
+    }
+
+
